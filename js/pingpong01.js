09ba574c52cbc41028d9465e63cc2ae218963153
ldk8 <41880019+ldk8@users.noreply.github.com>
2018-07-31 18:29:48 +0800

    球拍可以反弹小球

--- js/pingpong01.js
+++ js/pingpong01.js
@@ -73,6 +73,26 @@ function moveBall(){
 	}
 	ball.x+=ball.speed*ball.directionX;
 	ball.y+=ball.speed*ball.directionY;
+	//检测左侧球拍
+	var paddleAX=parseInt($("#paddleA").css("left"))+parseInt($("#paddleA").css("width"));
+	var paddleAYBottom=parseInt($("#paddleA").css("top"))+parseInt($("#paddleA").css("height"));
+	var paddleAYTop=parseInt($("#paddleA").css("top"));
+	if(ball.x+ball.speed*ball.directionX<paddleAX){
+		if(ball.y+ball.speed*ball.directionY<=paddleAYBottom&&
+			ball.y+ball.speed*ball.directionY>=paddleAYTop){
+			ball.directionX=1;
+		}
+	}
+	//检测右侧球拍
+	var paddleBX=parseInt($("#paddleB").css("left"));
+	var paddleBYBottom=parseInt($("#paddleB").css("top"))+parseInt($("#paddleB").css("height"));
+	var paddleBYTop=parseInt($("#paddleB").css("top"));
+	if(ball.x+ball.speed*ball.directionX>=paddleBX){
+		if(ball.y+ball.speed*ball.directionY<=paddleBYBottom&&
+			ball.y+ball.speed*ball.directionY>=paddleBYTop){
+			ball.directionX=-1;
+		}
+	}
 	$("#ball").css({
 		"left":ball.x,
 		"top":ball.y
