a6a501b4ab022f1f7e82a5c306a6f762a57ef268
ldk8 <41880019+ldk8@users.noreply.github.com>
2018-07-31 18:10:01 +0800

    小球可以反弹
    球拍可以键盘控制，小球触碰到边缘会反弹

--- js/pingpong01.js
+++ js/pingpong01.js
@@ -4,8 +4,17 @@ var KEY={
 	W:87,
 	S:83
 }
+
 var pingpong={};
 pingpong.pressedKeys=[];
+pingpong.ball={
+	speed:5,
+	x:150,
+	y:100,
+	directionX:1,
+	directionY:1
+}
+
 $(function(){
 	pingpong.timer=setInterval(gameloop,30);
 
@@ -18,9 +27,12 @@ $(function(){
 			pingpong.pressedKeys[e.which]=false;
 		});
 });
+
 function gameloop(){
+	moveBall();
 	movePaddles();
 };
+
 function movePaddles(){
 	if(pingpong.pressedKeys[KEY.UP]){
 		var top=parseInt($("#paddleB").css("top"));
@@ -38,4 +50,31 @@ function movePaddles(){
 		var top=parseInt($("#paddleA").css("top"));
 		$("#paddleA").css("top",top+5);
 	}
+};
+function moveBall(){
+	var playgroundWidth=parseInt($("#playground").width());
+	var playgroundHeight=parseInt($("#playground").height());
+	var ball=pingpong.ball;
+	//检测底边
+	if(ball.y+ball.speed*ball.directionY>playgroundHeight){
+		ball.directionY=-1;
+	}
+	//检测上边
+	if(ball.y+ball.speed*ball.directionY<0){
+		ball.directionY=1;
+	}
+	//检测底边
+	if(ball.x+ball.speed*ball.directionX>playgroundWidth){
+		ball.directionX=-1;
+	}
+	//检测底边
+	if(ball.x+ball.speed*ball.directionX<0){
+		ball.directionX=1;
+	}
+	ball.x+=ball.speed*ball.directionX;
+	ball.y+=ball.speed*ball.directionY;
+	$("#ball").css({
+		"left":ball.x,
+		"top":ball.y
+	});
 };
